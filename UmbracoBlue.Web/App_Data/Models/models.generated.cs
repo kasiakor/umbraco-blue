7c94f00c4248470b14a96e6ad4aaaa74c95d37e4
Kasia Kordys <kasia.kordys@gmail.com>
2020-04-09 20:13:09 +0100

    fix home view rendering

--- UmbracoBlue.Web/App_Data/Models/models.generated.cs
+++ UmbracoBlue.Web/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "94c2ee4bbd97f3d")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.8")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedModels
 {
