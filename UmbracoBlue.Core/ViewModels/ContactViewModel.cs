35928f4dc2b932d2973d2fb8437a723741c5b2b9
Kasia Kordys <kasia.kordys@gmail.com>
2020-04-21 17:15:31 +0100

    add contact view model

---
+++ UmbracoBlue.Core/ViewModels/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmbracoBlue.Core.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Enter your name" )]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter your email address")]
+        [EmailAddress(ErrorMessage = "Enter valid email address")]
+        public string EmailAdrress { get; set; }
+
+        [Required(ErrorMessage = "Enter your message")]
+        [MaxLength(500, ErrorMessage = "Your message must be no longer than 500 characters")]
+        public string Message { get; set; }
+    }
+}
