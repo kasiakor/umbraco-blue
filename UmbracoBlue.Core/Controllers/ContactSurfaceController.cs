fadea4386109e5ed045818a4c2a094065bed9fa5
Kasia Kordys <kasia.kordys@gmail.com>
2020-04-22 22:47:43 +0100

    add contact form controller

---
+++ UmbracoBlue.Core/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using UmbracoBlue.Core.ViewModels;
+
+namespace UmbracoBlue.Core.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            ContactViewModel model = new ContactViewModel() { ContactFormId = CurrentPage.Id };
+
+            return PartialView("~Views/Partials/Contact/contactForm.cshtml", model);
+         
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView("~Views/Partials/Contact/contactForm.cshtml", model);
+
+        }
+
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+
+            if (ModelState.IsValid)
+            {
+                success = SendMail();
+
+            }
+
+            var contactPage = UmbracoContext.Content.GetById(false, model.ContactFormId);
+            var successMessage = contactPage.Value<IHtmlString>("successMessage");
+            var errorMessage = contactPage.Value<IHtmlString>("errorMessage");
+            return PartialView("~Views/Partials/Contact/result.cshtml", success ? successMessage : errorMessage);
+        }
+        public bool SendMail()
+        {
+            return true;
+        }
+    }
+}
