e45886ecd5a875b187dc1b807c51c33a04b7572f
Kasia Kordys <kasia.kordys@gmail.com>
2020-04-24 17:45:19 +0100

    add contact form js

--- UmbracoBlue.Web/Scripts/contactForm.js
+++ UmbracoBlue.Web/Scripts/contactForm.js
@@ -1,1 +1,19 @@
-﻿
+﻿var contactForm = contactForm ||
+{
+    init: function () {
+        this.listeners();
+    },
+    listeners: function () {
+        $(document).on("click",
+            ".contact-submit",
+            function () {
+                e.preventDefault();
+                var form = $("#contact-form");
+                form.submit();     
+        });
+    },
+    showResult: function () {
+        $(".outer-form").hide("slow");
+        $(".form-results").show("slow");
+    }
+}
